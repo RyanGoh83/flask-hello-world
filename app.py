444da80a504c4b3b6af42a64781c5785e1ada217
Ryan Goh <gohry2@gmail.com>
2017-05-03 17:22:14 +0800

    added debug

--- app.py
+++ app.py
@@ -6,12 +6,48 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
+# Use decorator pattern to link view function to url.
 @app.route("/")
 @app.route("/hello")
 
+#define the view using a function, which returns a string
 def hello_world():
-	return "hello world!"
-	
+	return "hello world!?!?!?!?"
+
+#dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+        return search_query
+
+##@app.route("/integer/<int:value>")
+##def int_type(value):
+##        num = value + 1
+##        print (num)
+##        return "Correct!"
+##
+##@app.route("/float/<float:value>")
+##def float_type(value):
+##        print(value + 1)
+##        return "Correct!"
+##
+##@app.route("/path/<path:value>")
+##def path_type(value):
+##        print(value)
+##        return "Correct!"
+
+#dynamic route with explicit status codes
+@app.route("/name/<name>")
+def index(name):
+        if name.lower() == "michael":
+                return "Hello, {}".format(name), 200
+        else:
+                return "Not Found", 404
+        
+
+#start development server using the run() method
 if __name__ == "__main__":
 	app.run()
 	
